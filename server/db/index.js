55b588d3315d10e8de6373bba12c4e4406ec6721
Jerrod Varney <jerrodvarney@gmail.com>
2023-09-26 17:56:23 -0700

    database credential setup

--- server/db/index.js
+++ server/db/index.js
@@ -4,8 +4,8 @@ const Promise = require('bluebird');
 const database = 'shortly';
 
 const connection = mysql.createConnection({
-  user: 'student',
-  password: 'student'
+  user: 'root',
+  password: ''
 });
 
 const db = Promise.promisifyAll(connection, { multiArgs: true });
@@ -14,6 +14,7 @@ db.connectAsync()
   .then(() => console.log(`Connected to ${database} database as ID ${db.threadId}`))
   .then(() => db.queryAsync(`CREATE DATABASE IF NOT EXISTS ${database}`))
   .then(() => db.queryAsync(`USE ${database}`))
-  .then(() => createTables(db));
+  .then(() => createTables(db))
+  .catch(err => console.error('error connecting to database:', err));
 
 module.exports = db;
